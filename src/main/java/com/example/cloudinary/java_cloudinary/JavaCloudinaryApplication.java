df8503ffff463c066032b00a75f4456e3d310a01
eltatataa <davidtowo75@gmail.com>
2024-08-26 23:48:09 -0500

    setup - connection to the database

---
+++ src/main/java/com/example/cloudinary/java_cloudinary/JavaCloudinaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudinary.java_cloudinary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCloudinaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCloudinaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cloudinary/java_cloudinary/JavaCloudinaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudinary.java_cloudinary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCloudinaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
